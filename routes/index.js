baa914d1ddcf2618ef39c93795cb843583130491
Olivia Kasmin <oliviakasmin@gmail.com>
2020-08-15 10:19:33 -0400

    heroku: prepare to deploy

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
-const router = require('express').Router();
-const activityRoutes = require('./activities');
+const router = require('express').Router()
+const activityRoutes = require('./activities')
+const path = require('path')
 
-router.use('/api/activities', activityRoutes);
+router.use('/api/activities', activityRoutes)
 
-module.exports = router;
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+module.exports = router
