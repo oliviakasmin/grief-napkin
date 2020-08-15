0dc118b90460eb3ce47e9684a2c1454c67d24f68
Olivia Kasmin <oliviakasmin@gmail.com>
2020-08-15 11:05:00 -0400

    package json

--- config/index.js
+++ config/index.js
@@ -6,6 +6,4 @@ const mongoDbPassword = process.env.MONGO_DB_PASSWORD
 
 const MONGODB_URI = `mongodb+srv://${mongoDbUsername}:${mongoDbPassword}@cluster0.ts2fv.mongodb.net/test?retryWrites=true&w=majority`
 
-// const MONGODB_URI = `mongodb+srv://${mongoDbUsername}:${mongoDbPassword}@cluster0.ts2fv.mongodb.net/test?retryWrites=true&w=majority`;
-
 module.exports = MONGODB_URI
